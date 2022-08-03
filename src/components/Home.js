37249f9da99391f84f2e857c8eef41f9175b73a7
Olivia <oliviatmanalastas@gmail.com>
2022-08-03 17:36:36 -0400

    Added header content

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,23 @@ import React from "react";
 
 const Home = () => {
    return (
-      <div className="home-header">
-         <h1></h1>
+      <div className="home-main-container">
+         <div className="home-header">
+            <h1 className="home-head">OTM</h1>
+            <h3 className="home-body">An up-and-coming tech-startup in <br></br> New York City <br></br>
+               that aims to provide you a better and smoother commute.<br></br>
+               How? Well, by informing you the precise and accurate time of
+               the subway's arrival
+            </h3>
+         </div>
+         <div className="home-main-body">
+            <h2>Note:</h2>
+            <h3 className="home-body">This is a work-in-progress. What you're about to see only
+               includes the <strong>Subway Station</strong> and the 
+               <strong>Arrival Time</strong>.
+            </h3>
+            <h3>Again, this is a work in progress</h3>
+         </div>
       </div>
    )
 }
