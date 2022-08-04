081b71a99de022fe144cd7184248066401d771e1
Olivia <oliviatmanalastas@gmail.com>
2022-08-04 15:01:18 -0400

    CSS styles added to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,22 +7,24 @@ const Home = () => {
             <div className="head-circle">
                <h1 className="home-head">OMT</h1>
             </div>
-            <div className="home-body-container">
-
+            <div className="home-body">
+               <h1 className="home-body-header">WHAT WE DO</h1>
+               <h3 className="home-body-text">An up-and-coming tech-startup in New York City
+                  that aims to provide you a better and smoother commute.<br></br>
+                  How? Well, by informing you <br></br>the precise and accurate time of
+                  the subway's arrival
+               </h3>
             </div>
-            <h3 className="home-body">An up-and-coming tech-startup in <br></br> New York City <br></br>
-               that aims to provide you a better and smoother commute.<br></br>
-               How? Well, by informing you the precise and accurate time of
-               the subway's arrival
-            </h3>
          </div>
          <div className="home-main-body">
-            <h2>Note:</h2>
-            <h3 className="home-body">This is a work-in-progress. What you're about to see only
-               includes the <strong>Subway Station</strong> and the 
-               <strong>Arrival Time</strong>.
-            </h3>
-            <h3>Again, this is a work in progress</h3>
+            <h2 className="body-1">NOTE:</h2>
+            <div className="home-second-body">
+               <h3 className="body-2">This is a work-in-progress. <br></br> What you're about to see only
+                  includes the <strong>Subway Station </strong> and the
+                  <strong> Arrival Time </strong>.
+               </h3>
+               <h3 className="body-3">Again, this is a work in progress</h3>
+            </div>
          </div>
       </div>
    )
